7a45ef64161f28d195c9299a64d69c30aaa3b798
Tom <mr@tomclement.nl>
2021-12-21 15:21:08 +0100

    Trying to move all build steps to python3

--- write_zip_sha2017.sh
+++ write_zip_sha2017.sh
@@ -5,4 +5,4 @@ cd first_boot_files
 zip -0 ../files.zip -xi *
 cd ..
 
-python2 esp-idf/components/esptool_py/esptool/esptool.py --port /dev/ttyUSB0 write_flash 0x191000 files.zip
+python3 esp-idf/components/esptool_py/esptool/esptool.py --port /dev/ttyUSB0 write_flash 0x191000 files.zip
