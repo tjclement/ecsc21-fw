6b897d7935f8c55979b0abe4bb6b2b721e6912da
Tom <mr@tomclement.nl>
2021-12-24 17:10:18 +0100

    Fixed issue where saving machine code into IRAM buffer hardfaulted

--- firmware/components/micropython/esp32/modrawexec.c
+++ firmware/components/micropython/esp32/modrawexec.c
@@ -10,22 +10,33 @@ typedef int (*printf_ptr) (const char *str, ...);
 typedef void (*assembly_ptr) (const char *str, printf_ptr);
 typedef void (*simple_ptr) ();
 
-static IRAM_ATTR uint8_t *assembly;
+static IRAM_ATTR uint8_t assembly[4096] = {0};
 
 
 STATIC mp_obj_t rawexec_call_(mp_obj_t _text, mp_obj_t _assembly) {
     mp_uint_t text_len;
     const char *text = (char *) mp_obj_str_get_data(_text, &text_len);
 
-    mp_uint_t assembly_len;
-    assembly = (uint8_t *) mp_obj_str_get_data(_assembly, &assembly_len);
-
     bool is_bytes = MP_OBJ_IS_TYPE(_assembly, &mp_type_bytes);
     if (!is_bytes) {
         mp_raise_msg(&mp_type_AttributeError, "Assembly should be a bytestring");
     }
 
-    simple_ptr call_assembly = &assembly;
+    // Workaround for issue where IRAM_ATTR uint8_t array is not saved 4-byte
+    // aligned in memory.
+    uint8_t* aligned_assembly = assembly;
+    while(aligned_assembly % 4 !== 0) {
+      aligned_assembly++;
+    }
+
+    mp_uint_t assembly_len;
+    uint8_t* assembly_pointer = (uint8_t *) mp_obj_str_get_data(_assembly, &assembly_len);
+    printf("assembly_len: %d, assembly location: %p, first dword: %04X\n", assembly_len, aligned_assembly, *((uint32_t*)assembly_pointer));
+    fflush(stdout);
+
+    memcpy(aligned_assembly, assembly_pointer, assembly_len);
+
+    simple_ptr call_assembly = aligned_assembly;
     call_assembly();
 
     return mp_const_none;
