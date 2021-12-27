4a6e29595fc82e2b9ab6a4dd753d087c0c7916e5
Remco Wendt <r.wendt@ultimaker.com>
2021-12-27 07:56:52 +0100

    Document solution to challenge 1a

--- firmware/python_modules/icsc2021/challenges/1a.py
+++ firmware/python_modules/icsc2021/challenges/1a.py
@@ -10,6 +10,24 @@ def secure_read_efuse_block(block, start_offset, length):
 
 
 def get_flag(bytes):
+    """
+    *** SOLUTION ***
+
+    1.  Use the logic flaw in `secure_read_efuse_block()` to bypass the 'security'
+        by providing the right block and offset with a length of 0:
+
+    >>> secure_read_efuse_block(efuse.EFUSE_BLK2, 0, 0)
+    b'P\xd1\xf7\xf8\xcb]\xe7e\xcc\x0e\x91\n\x90\x8b\xa4\x82*\x08^\xf8I\x1d\x1f\xee\x00\x00\x00\x00\x00\x00\x00\x00'
+    >>> get_flag(b'P\xd1\xf7\xf8\xcb]\xe7e\xcc\x0e\x91\n\x90\x8b\xa4\x82*\x08^\xf8I\x1d\x1f\xee')
+    CTF{50d1f7f8cb5de765cc0e910a908ba4822a085ef8491d1fee}
+
+    2.  Find out that the underlying call to `efuse.read_block()` can be used directly:
+
+    >>> efuse.read_block(efuse.EFUSE_BLK2, 0, 256)
+    b'P\xd1\xf7\xf8\xcb]\xe7e\xcc\x0e\x91\n\x90\x8b\xa4\x82*\x08^\xf8I\x1d\x1f\xee\x00\x00\x00\x00\x00\x00\x00\x00'
+    >>> get_flag(b'P\xd1\xf7\xf8\xcb]\xe7e\xcc\x0e\x91\n\x90\x8b\xa4\x82*\x08^\xf8I\x1d\x1f\xee')
+    CTF{50d1f7f8cb5de765cc0e910a908ba4822a085ef8491d1fee}
+    """
     if len(bytes) != 24:
         print("That's not valid flag data, a flag consists of 24 bytes.")
     else:
