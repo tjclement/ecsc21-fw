405519c411fc7d34ee40547e5cfcaede839a9f5f
Remco Wendt <r.wendt@ultimaker.com>
2021-12-27 07:55:20 +0100

    Fix formatting of 1a

--- firmware/python_modules/icsc2021/challenges/1a.py
+++ firmware/python_modules/icsc2021/challenges/1a.py
@@ -3,7 +3,7 @@ import flags, easydraw, efuse, binascii
 
 def secure_read_efuse_block(block, start_offset, length):
     if block == efuse.EFUSE_BLK2 and length > 0:
-        print('No access allowed to secure efuse region!')
+        print("No access allowed to secure efuse region!")
     else:
         length = length if length > 0 else 256
         print(efuse.read_block(block, start_offset, length))
@@ -13,27 +13,27 @@ def get_flag(bytes):
     if len(bytes) != 24:
         print("That's not valid flag data, a flag consists of 24 bytes.")
     else:
-        print('CTF{%s}' % str(binascii.hexlify(bytes), 'ascii'))
+        print("CTF{%s}" % str(binascii.hexlify(bytes), "ascii"))
 
 
 _message_ui = (
-    'Connect to the device via USB at baud 115200. You\'re now in a Python shell.\n'
-    'You can paste snippets using CTRL+E and CTRL+D.\n\n'
-    'Extract the flag from efuse by calling secure_read_efuse_block(<block>, <start_offset>, <length>)\n'
-    'Use get_flag(<bytes>) to convert found data into a flag.\n\n'
+    "Connect to the device via USB at baud 115200. You're now in a Python shell.\n"
+    "You can paste snippets using CTRL+E and CTRL+D.\n\n"
+    "Extract the flag from efuse by calling secure_read_efuse_block(<block>, <start_offset>, <length>)\n"
+    "Use get_flag(<bytes>) to convert found data into a flag.\n\n"
     'You can submit the flag by calling flags.submit_flag("CTF{xxxx}").'
 )
 
 _message_console = (
-    _message_ui + '\n\n'
-    'To show off our 1337 skills we have included part of the implementation here:\n'
-    '''
+    _message_ui + "\n\n"
+    "To show off our 1337 skills we have included part of the implementation here:\n"
+    """
     def secure_read_efuse_block(block, start_offset, length):
         if block == efuse.EFUSE_BLK2 and length > 0:
             print('No access allowed to secure efuse region!')
         else:
             length = length if length > 0 else 256
-    '''
+    """
 )
 
 print(_message_console)
