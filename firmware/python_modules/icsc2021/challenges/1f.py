c31148214779f9e75bc6450d06ea863bc0d14dc9
Remco Wendt <r.wendt@ultimaker.com>
2021-12-27 07:09:03 +0100

    ecsc21-fw-19: Implement challenge 1f. Closes #19

--- firmware/python_modules/icsc2021/challenges/1f.py
+++ firmware/python_modules/icsc2021/challenges/1f.py
@@ -1,30 +1,22 @@
-import flags, display, easydraw
+import flags, easydraw, rawexec
 
 
-def encrypt(plaintext):
-    if type(plaintext) == str:
-        plaintext = plaintext.encode("ascii")
-    if type(plaintext) != bytes:
-        print("You can only encrypt strings or byte strings.")
-        return
-
-    plaintext = bytearray(plaintext)
-
-    key = b"\x57\x15\x71\xd8\x3c\xee\x2e\x79\x3a\x7c\xc7\x5a\x33\x34\xfb\x00"
-    key_length = len(key)
-    for index, char in enumerate(plaintext):
-        plaintext[index] ^= key[index % key_length]
-
-    return bytes(plaintext)
+def pass_flag(instructions):
+    tha_flag = "CTF{0a977ec472ed1d54a0408fd9f46dd76cb85196c4d3175384}\n"
+    rawexec.call(tha_flag, instructions)
 
 
-_crypt_flag = b'\x14A7\xa3\t\x8a\x1c\x1d\\\x1d\xff;Q\r\xc32n"\x14\xbbY\xd8K\x1f^O\xf4c\x00\x05\xc3db"E\xbb\x04\xd7J\x1f\x0f\x1e\xf28\x00\x07\xce43\'F\xe1A'
 _message_ui = (
-    'This flag shown in your terminal was encrypted using perfect security.\n\nThe scheme is so secure, that we\'ll allow you to test it yourself by calling encrypt("yourphrase").\n\n'
-    + 'You can submit the decrypted flag by calling flags.submit_flag("CTF{xxxx}").'
+    "Get the flag by calling pass_flag(<instructions>).\n\n"
+    "Instructions should be sent as a bytesting of Xtensa Tensilica machine code "
+    "that gets executed as a C function: instructions(flag, printf)\n\n"
+    "You can paste snippets using CTRL+E and CTRL+D.\n\n"
+    "You can submit the flag by calling flags.submit_flag('CTF{xxxx}')."
 )
 
-display.drawFill(0x0)
-easydraw.messageCentered("One Time Pwn\n\n\n" + _message_ui)
+_message_console = (
+    _message_ui
+)
 
-print("Encrypted flag: ", str(_crypt_flag))
+print(_message_console)
+easydraw.messageCentered("Awesome ASM\n\n\n" + _message_ui)
